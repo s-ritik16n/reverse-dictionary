731aac16130a51c92453aac7da210d6a9099c09b
Ritik Saxena <saxenar444@gmail.com>
2018-01-29 22:24:32 +0530

    removed stopwords from definitions

--- venv/src/data_ops/clean_definitions.py
+++ venv/src/data_ops/clean_definitions.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from connect_db import get_engine
 from print_error import print_error
+from nltk.corpus import stopwords
 
+stopwords = stopwords.words('english')
 engine, meta = get_engine()
 definitions = pd.read_sql("definitions", engine)
 
-def stem():
-    pass
+def stem(definition):
+	pass    
 
-def remove_stopwords():
-    pass
+def remove_stopwords(definition):
+    return ' '.join([d for d in definition.split() if d not in stopwords])
     
-definitions['definition'] = definitions.apply()   # tem
-definitions['definition'] = definitions.apply()   # remove stepwords
+#definitions['definition'] = definitions['definition'].apply(stem)   # stem
+definitions['definition'] = definitions['definition'].apply(remove_stopwords)   # remove stepwords
+definitions.to_sql("definitions_new", engine,if_exists='append', index=False)
