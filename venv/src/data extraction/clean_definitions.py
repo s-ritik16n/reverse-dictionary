140ac1f596c5d39cc057d122653f331af689d3cc
Ritik Saxena <saxenar444@gmail.com>
2018-02-05 23:07:52 +0530

    next task - remove null entries

--- venv/src/data extraction/clean_definitions.py
+++ venv/src/data extraction/clean_definitions.py
@@ -3,27 +3,33 @@ from connect_db import get_engine
 from print_error import print_error
 from nltk.corpus import stopwords
 
-stopwords = stopwords.words('english')
-engine, meta = get_engine()
-definitions = pd.read_csv("db/definitions.csv", sep=":", names = ["word","definition"])
-words = []
+stopwords = stopwords.words("english")
 
-def stem(definition):
-	pass    
+engine, meta = get_engine()
+df = pd.read_csv("db/definitions.csv", sep=":", names = ["word","definition"])
+words = pd.DataFrame(columns = ["word"], index = None)
+
+print(df.info())
 
 def remove_stopwords(definition):
-    return ' '.join([d for d in definition.split() if d not in stopwords])
+	return ' '.join([d for d in definition.split() if d not in stopwords])
 
-def stop_words_main():
-	#definitions['definition'] = definitions['definition'].apply(stem)   # stem
-	definitions['definition'] = definitions['definition'].apply(remove_stopwords)   # remove stepwords
-	definitions.to_csv("db/definitions.csv", sep=":", header=False, index=False, mode="w")
+def more_words(definition):
+	word = [d for d in definition.split() if not any(df["word"] == word)]
+	for w in word:
+		words.append({"word":w}, ignore_index=True)
 
-def get_non_matching_words(definition):
-	words.extend(d for d in definition.split() if d not in definitions['word'] and d not in words)
+def main():
+	global df
+	df = df.dropna(axis=1, how='any')
+	print("*"*5)
+	print(df.count())
+	df.to_csv("db/definitions.csv", sep=":", columns=["word","definitions"], mode="w")
+	#df['definition'] = df['definition'].apply(remove_stopwords)
+	#print("*"*5)
+	#print(df.info())
+	#df.to_csv("db/definitions.csv", sep = ":", names=["word","definition"], mode="w")
+	#df['definition'].apply(more_words)
+	#words.to_csv("db/remaining_words.csv", names = ["word"], mode="w")
 
-def common_words_main():
-	definitions['definition'].apply(get_non_matching_words)
-
-common_words_main()
-print(len(words))
+main()
