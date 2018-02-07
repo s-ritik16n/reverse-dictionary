51d182869c7a387875e410874eb82e39a1050e8e
Ritik Saxena <saxenar444@gmail.com>
2018-02-07 21:36:49 +0530

    resolved null and 'null' conflict using keep_default_na

--- venv/src/data extraction/extract_null_words.py
+++ venv/src/data extraction/extract_null_words.py
@@ -5,7 +5,7 @@ extract words whose definition is not present in defiitions.csv
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("db/definitions.csv", names=["word","definition"], sep=":", index_col=None)
+df = pd.read_csv("db/definitions.csv", names=["word","definition"], sep=":", index_col=None, keep_default_na=False, na_values=[""])
 words = pd.DataFrame(columns=["word"], index=None)
 
 indices = []
@@ -18,9 +18,8 @@ for id,w in enumerate(ww):
 	if w is True:
 		print(df.loc[id][0], df.loc[id][1])
 
-print("*"*10)
-
 dd = pd.isnull(df['definition'])
 for id,d in enumerate(dd):
 	if d is True:
 		print(df.loc[id][0], df.loc[id][1])
+
