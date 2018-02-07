b89234eb2b9160f31036da60603eeb8424e108ab
Ritik Saxena <saxenar444@gmail.com>
2018-02-07 21:54:57 +0530

    definitions.csv null values removed

--- venv/src/data extraction/extract_null_words.py
+++ venv/src/data extraction/extract_null_words.py
@@ -14,12 +14,30 @@ def null(word):
 		indices.append(word)
 
 ww = pd.isnull(df['word'])
+null_word_indices = []
 for id,w in enumerate(ww):
 	if w is True:
-		print(df.loc[id][0], df.loc[id][1])
+		null_word_indices.append(id)
+		# print(df.loc[id][0], df.loc[id][1])
 
 dd = pd.isnull(df['definition'])
+null_def_indices = []
 for id,d in enumerate(dd):
 	if d is True:
-		print(df.loc[id][0], df.loc[id][1])
+		null_def_indices.append(id)
+		# print(df.loc[id][0], df.loc[id][1])
 
+for el in null_word_indices:
+	if el in null_def_indices:
+		null_def_indices.remove(el)
+
+print("df info before deletion")
+print(df.info())
+print("*"*10)
+
+df = df.drop(null_word_indices, inplace=False)
+df = df.drop(null_def_indices, inplace=False)
+df = df.reset_index(drop=True)
+print(df.info())
+
+df.to_csv("db/definitions.csv", sep=":", columns=["word","definition"], header=False, index=False, mode="w")
